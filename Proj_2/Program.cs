6650c4241f887a3b229af6a2799197d10b204d1b
Maximiliam Viklund <maximiliam.viklund@elev.ga.ntig.se>
2023-08-31 10:59:13 +0200

    Basic skelett

---
+++ Proj_2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+string resp;
+
+Console.WriteLine("This is *Proj2* an interactive story, chosse from the given choices and progress.");
+Console.WriteLine("Lets try it!");
+Console.WriteLine("Do you understand?");
+Console.WriteLine("(y)Yea/(n)No");
+resp = Console.ReadLine();
+if(resp == "n"){
+    Console.WriteLine("Alright then F off.");
+}
+else if(resp == "y"){
+    Console.WriteLine("Great!.");
+
+    Console.WriteLine("xxx");
+    Console.WriteLine("Do you a)xxx b)yyy c)zzz");
+    resp = Console.ReadLine();
+
+    if(resp == "b"){
+
+    }
+    else if(resp == "c"){
+
+    }
+    else if(resp == "a"){
+
+    }
+    else{
+        Console.WriteLine("Chosse a viable answer next time.");
+    }
+}
+
+
+Console.ReadLine();
